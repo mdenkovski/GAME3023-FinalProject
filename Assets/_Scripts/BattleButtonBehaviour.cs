c8fedc4d98551ac85c11ab47fbf89e71b68ed3cb
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-12-09 17:33:59 -0500

    added debug buttons for transitions

---
+++ Assets/_Scripts/BattleButtonBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class BattleButtonBehaviour : MonoBehaviour
+{
+    public void OnBattleButtonPressed()
+    {
+        Debug.Log("Battle Button Pressed");
+        SceneManager.LoadScene("Play");
+    }
+}
