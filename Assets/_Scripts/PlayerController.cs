00c71006655fd2a4c56fce9eb4f7d21916a87a64
woolenelk <chua.kevin.christopher@gmail.com>
2020-12-10 15:55:18 -0500

    Don't destroy on load

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         SavePositionButtonBehaviour.OnSave.AddListener(OnSave);
         LoadPosition();
+        DontDestroyOnLoad(gameObject);
     }
 
 
