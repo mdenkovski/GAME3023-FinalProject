47a739ad7cb6e6b38f3648179add74e04c764a61
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-12-10 00:33:36 -0500

    decoupled the encounter manager from the player controller

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -10,29 +10,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidBody;
 
-    [Header("Encounters")]
-    [SerializeField]
-    [Range(0, 100)]
-    float encounterChance;
+    
 
-    [SerializeField]
-    Transform groundCheckTransform;
-
-    [SerializeField]
-    LayerMask EncounterLayer;
-
-    [SerializeField]
-    float encnounterCheckFrequency;
-
-    [Header("BattleTransition")]
-    [SerializeField]
-    BattleTransitionManager battleTransitionManager;
+    
 
     private void Start()
     {
         SavePositionButtonBehaviour.OnSave.AddListener(OnSave);
         LoadPosition();
-        InvokeRepeating("OnTallGrass", 1.0f, encnounterCheckFrequency);
+        
         DontDestroyOnLoad(gameObject);
     }
 
@@ -72,33 +58,7 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    private void OnTallGrass()
-    {
-        if (rigidBody.velocity.magnitude >= 0.1f)
-        {
-            Collider2D collision = Physics2D.OverlapCircle(groundCheckTransform.position, 0.2f, EncounterLayer);
-
-            if (collision)
-            {
-                float roll = Random.Range(0.0f, 100.0f);
-                //Debug.Log("Rolled a: " + roll);
-                if(roll <= encounterChance)
-                {
-                    battleTransitionManager.EnterEncounter();
-                    
-                }
-
-            }
-        }
-    }
-
-    //visualize the shpoere for the overlap circle check
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.white;
-
-        Gizmos.DrawWireSphere(groundCheckTransform.position, 0.2f);
-    }
+    
 
    
 }
